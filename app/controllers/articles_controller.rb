8ebb700b2261eb5a87524b405f77585043d0ab17
tozzr <mail@tozzr.com>
2014-11-01 09:53:34 +0100

    generic crud controller specs for articles_controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_filter :check_authorization
 
   # GET /articles
   # GET /articles.json
@@ -28,7 +29,7 @@ class ArticlesController < ApplicationController
     respond_to do |format|
       if @article.save
         format.html { redirect_to @article, notice: 'Article was successfully created.' }
-        format.json { render :show, status: :created, location: @article }
+        format.json { render json: @article, status: :created, location: @article }
       else
         format.html { render :new }
         format.json { render json: @article.errors, status: :unprocessable_entity }
@@ -63,7 +64,14 @@ class ArticlesController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article
-      @article = Article.find(params[:id])
+      begin
+        @article = Article.find(params[:id])
+      rescue Exception => e
+        respond_to do |format|
+          format.html { redirect_to articles_url }
+          format.json { head :not_found }
+        end
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,186 +1,35 @@
 require 'rails_helper'
 
 describe ArticlesController do
-  fixtures :articles
-
-  describe "GET 'index'" do
-    it "is successful" do
-      get :index
-      expect(response).to be_success
-    end
-
-    it "assigns @articles" do
-      get :index
-      expect(assigns(:articles)).to_not be_nil
-    end
-
-    it "renders index template" do
-      get :index
-      expect(response).to render_template('index')
-    end
-  end
-  
-  describe "GET 'show'" do
-    before(:each) do
-      @article = articles(:one)
-    end
-
-    it 'is successful' do
-      get :show, :id => @article.id
-      expect(response).to be_success
-    end
-
-    it "assigns @article" do
-      get :show, :id => @article.id
-      expect(assigns(:article)).to_not be_nil
-    end
-
-    it "renders the 'show' template" do
-      get :show, :id => @article.id
-      expect(response).to render_template('show')
-    end
-  end  
-
-  describe "GET 'new'" do
-    it 'is successful' do
-      get :new
-      expect(response).to be_success
-    end
-
-    it "assigns @article" do
-      get :new
-      expect(assigns(:article)).to_not be_nil
-    end
-
-    it "renders the 'new' template" do
-      get :new
-      expect(response).to render_template('new')
-    end
-  end  
-
-  describe "POST 'create'" do
-    before(:each) do
-      @article = Article.new
-      allow(@article).to receive(:id).and_return(1)
-      allow(@article).to receive(:title).and_return('foo')
-    end
-
-    context "The save is successful" do
-      before(:each) do
-        expect(Article).to receive(:new).and_return(@article)
-        expect(@article).to receive(:save).and_return(true)
-      end
-
-      it "redirects to the 'show' action" do
-        post :create, :article => @article.attributes
-        expect(response).to redirect_to @article # Put the right show path here
-      end
-
-      it "sets a flash message" do
-        post :create, :article => @article.attributes
-        expect(flash[:notice]).to be == 'Article was successfully created.'
-      end
-    end
-
-    context "the save fails" do
-      before(:each) do
-        expect(@article).to receive(:save).and_return(false)
-        expect(Article).to receive(:new).and_return(@article)
-      end
-
-      it "renders the 'new' action" do
-        post :create, :article => @article.attributes
-        expect(response).to render_template(:new)
-      end
-
-      it "assigns @article" do
-        post :create, :article => @article.attributes
-        expect(assigns(:article)).to_not be_nil
-      end
-    end
-  end 
-
-  describe "GET 'edit'" do
-    before(:each) do
-      @article = articles(:one)
-    end
-
-    it 'is successful' do
-      get :edit, :id => @article.id
-      expect(response).to be_success
-    end
-
-    it "assigns @article" do
-      get :edit, :id => @article.id
-      expect(assigns(:article)).to_not be_nil
-    end
-
-    it "renders the 'edit' template" do
-      get :edit, :id => @article.id
-      expect(response).to render_template('edit')
-    end
-  end  
-
-  describe "PUT 'update'" do
-    before(:each) do
-      @article = articles(:one)
-    end
-
-    context "the update is successful" do
-      before(:each) do
-        expect(@article).to receive(:update).and_return(true)
-        expect(Article).to receive(:find).with(@article.id.to_s).and_return(@article)
-      end
-
-      it "redirects to 'show' action" do
-        put :update, :id => @article.id, :article => {title: 'some title', text: 'some text'} # Add here some attributes for the model
-        expect(response).to redirect_to(article_path(@article)) # Put the right show path here
-      end
-
-      it "sets a flash message" do
-        put :update, :id => @article.id, :article => {title: 'some title', text: 'some text'} # Add here some attributes for the model
-        expect(flash[:notice]).to be == 'Article was successfully updated.' # Your flash message here
-      end
-    end
-
-    context "the update fails" do
-      before(:each) do
-        expect(@article).to receive(:update).and_return(false)
-        expect(Article).to receive(:find).with(@article.id.to_s).and_return(@article)
-      end
-
-      it "renders the 'edit' action" do
-        put :update, :id => @article.id, :article => {title: 'some title', text: 'some text'} # Add here some attributes for the model
-        expect(response).to render_template(:edit)
-      end
-
-      it "assigns @article" do
-        put :update, :id => @article.id, :article => {title: 'some title', text: 'some text'} # Add here some attributes for the model
-        expect(assigns(:article)).to_not be_nil
-      end
-    end
-  end 
-
-  describe "DELETE 'destroy'" do
-    before(:each) do
-      @article = articles(:one)
-      expect(Article).to receive(:find).with(@article.id.to_s).and_return(@article)
-    end
-
-    it "should delete the article" do
-      expect(@article).to receive(:destroy).and_return(true)
-      delete :destroy, :id => @article.id
-    end
-
-    it "should redirect to index page" do
-      delete :destroy, :id => @article.id
-      expect(response).to redirect_to(:articles)
-    end
-
-    it "sets a flash message" do
-      delete :destroy, :id => @article.id
-      expect(flash[:notice]).to be == 'Article was successfully destroyed.' # Your flash message here
-    end
+  before(:each) do
+    @model = "Article"
   end
 
+  context "not logged in" do
+    before(:each) do
+      session[:userId] = nil
+    end
+
+    it_should_behave_like "GET index redirects to login"
+    it_should_behave_like "GET show redirects to login"
+    it_should_behave_like "GET new redirects to login"
+    it_should_behave_like "POST create redirects to login"
+    it_should_behave_like "GET edit redirects to login"
+    it_should_behave_like "PUT update redirects to login"
+    it_should_behave_like "DELETE destroy redirects to login"
+  end
+
+  context "logged in" do
+    before(:each) do
+      session[:userId] = 1
+    end
+
+    it_should_behave_like "CRUD GET index"
+    it_should_behave_like "CRUD GET show"
+    it_should_behave_like "CRUD GET new"
+    it_should_behave_like "CRUD POST create"
+    it_should_behave_like "CRUD GET edit"
+    it_should_behave_like "CRUD PUT update"
+    it_should_behave_like "CRUD DELETE destroy"
+  end
 end
